23f54d335e38a53c74e7ad189341caf427b4c272
Jason Whitted <JWhitted@ent.com>
2017-12-18 12:29:49 -0700

    Add onChange prop to TextField

--- src/common/components/TextField/TextField.js
+++ src/common/components/TextField/TextField.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const TextField = ({ id, label, name, value }) => (
+const TextField = ({ id, label, name, value, onChange }) => (
   <div className="form-group">
     <label htmlFor={id || name}>{label}</label>
-    <input id={id || name} name={name} type="text" className="form-control" value={value} />
+    <input id={id || name}
+           name={name}
+           type="text"
+           className="form-control"
+           value={value}
+           onChange={onChange} />
   </div>
 );
 
@@ -13,6 +18,7 @@ TextField.propTypes = {
   label: PropTypes.string.isRequired,
   name: PropTypes.string.isRequired,
   value: PropTypes.oneOfType([PropTypes.number, PropTypes.string]).isRequired,
+  onChange: PropTypes.func.isRequired,
 };
 
 TextField.defaultProps = {
