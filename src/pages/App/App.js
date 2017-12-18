ca9213b60f4a14be63fa41ca85f2add67bf77144
Jason Whitted <JWhitted@ent.com>
2017-12-18 08:44:23 -0700

    Refactor project directory

---
+++ src/pages/App/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to React</h1>
+        </header>
+        <p>Hello World</p>
+      </div>
+    );
+  }
+}
+
+export default App;
