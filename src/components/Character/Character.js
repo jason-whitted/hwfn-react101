f77de872017af6526cd12cc1017cefb8e6c3dcf7
Jason Whitted <JWhitted@ent.com>
2017-12-18 14:21:44 -0700

    Update Character to display name/thumbnail

--- src/components/Character/Character.js
+++ src/components/Character/Character.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Character = ({ character }) => (
-  <div>
-    Character
-    <pre>{JSON.stringify(character, null, 2)}</pre>
-  </div>
-);
+const Character = ({ character }) => {
+  const { name, realm, thumbnail } = character || {};
+
+  return (
+    <div>
+      {character && (
+        <div>
+          <h1>{realm} - {name}</h1>
+          <img src={`https://render-us.worldofwarcraft.com/character/${thumbnail}`} alt={name}/>
+          <pre>{JSON.stringify(character, null, 2)}</pre>
+        </div>
+      )}
+    </div>
+  );
+};
 
 Character.propTypes = {
   character: PropTypes.object,
