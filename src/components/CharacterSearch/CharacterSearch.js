36ab88e8037f83d173379decd8c9d60106f4fe36
Jason Whitted <JWhitted@ent.com>
2017-12-18 12:51:19 -0700

    Add onChange to CharacterSearch

--- src/components/CharacterSearch/CharacterSearch.js
+++ src/components/CharacterSearch/CharacterSearch.js
@@ -1,12 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { TextField } from 'common';
 
-const CharacterSearch = () => (
-  <div>
-    <TextField name="realm" label="Realm:" value="Sargeras" />
-    <TextField name="character" label="Character:" value="Stizzasaurus" />
-  </div>
-);
+class CharacterSearch extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      realm: 'Sargeras',
+      character: 'Stizzasaurus',
+    };
+  }
+
+  onChange = event => {
+    const { name, value } = event.target;
+    this.setState({ [name]: value });
+  };
+
+  render = () => {
+    const { realm, character } = this.state;
+
+    return (
+      <div>
+        <TextField name="realm" label="Realm:" value={realm} onChange={this.onChange} />
+        <TextField name="character" label="Character:" value={character} onChange={this.onChange} />
+      </div>
+    );
+  };
+}
 
 export default CharacterSearch;
