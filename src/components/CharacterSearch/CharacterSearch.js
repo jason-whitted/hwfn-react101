d13e94f7d6bb479d6bb340858c56f010278b5035
Jason Whitted <JWhitted@ent.com>
2017-12-18 13:05:53 -0700

    Add submitHandler to CharacterSearch

--- src/components/CharacterSearch/CharacterSearch.js
+++ src/components/CharacterSearch/CharacterSearch.js
@@ -16,13 +16,21 @@ class CharacterSearch extends Component {
     this.setState({ [name]: value });
   };
 
+  submitHandler = event => {
+    console.log('CharacterSearch', 'submitHandler', event);
+    event.preventDefault();
+  }
+
   render = () => {
     const { realm, character } = this.state;
 
     return (
       <div>
-        <TextField name="realm" label="Realm:" value={realm} onChange={this.onChange} />
-        <TextField name="character" label="Character:" value={character} onChange={this.onChange} />
+        <form onSubmit={this.submitHandler}>
+          <TextField name="realm" label="Realm:" value={realm} onChange={this.onChange} />
+          <TextField name="character" label="Character:" value={character} onChange={this.onChange} />
+          <button className="btn btn-primary">Submit</button>
+        </form>
       </div>
     );
   };
