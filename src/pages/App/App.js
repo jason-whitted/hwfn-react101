64b45119e2813ddc4999fb424e911de37a878e5b
Jason Whitted <JWhitted@ent.com>
2017-12-19 11:11:06 -0700

    Implement react-router

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,32 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import { Character, Mounts, RealmStatus } from 'pages';
-
-const route = () => {
-  switch(document.location.pathname) {
-    case '/character':
-      return <Character />;
-    case '/mounts':
-      return <Mounts />;
-    case '/':
-    case '/realmstatus':
-      return <RealmStatus />;
-    default:
-      return <div>404 - Uh oh o.O</div>;
-  }
-}
 
 class App extends Component {
   render() {
     return (
-      <div className="container">
-        <header>
-          <h1>Battle.not</h1>
-        </header>
-        <div className="row">
-          {route()}
+      <Router>
+        <div className="container">
+          <header>
+            <h1>Battle.not</h1>
+          </header>
+          <div className="row">
+            <Switch>
+              <Route exact path="/" component={RealmStatus} />
+              <Route exact path="/character" component={Character} />
+              <Route exact path="/mounts" component={Mounts} />
+              <Route exact path="/realmstatus" component={RealmStatus} />
+              <Route render={() => <div>404 - Not found o.O</div>} />
+            </Switch>
+          </div>
         </div>
-      </div>
+      </Router>
     );
   }
 }
