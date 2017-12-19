dc5e9469435d4bbb1bcc9b13d0cf0db87d746d67
Jason Whitted <JWhitted@ent.com>
2017-12-19 16:53:24 -0700

    Implement withTotal on Thing component

--- src/components/Thing/Thing.js
+++ src/components/Thing/Thing.js
@@ -1,24 +1,44 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 
-const Thing = ({ increment }) => (
-  <div>
-    <h1>Thing (by {increment})</h1>
-    <div>
-      <label>Count:</label>
-    </div>
-    <button className="btn btn-default">
-      Add
-    </button>
-  </div>
-);
+import { withTotal } from 'hocs';
+
+class Thing extends Component {
+  click = event => {
+    const { add, increment } = this.props;
+    add(increment);
+  };
+
+  render = () => {
+    const { increment, total } = this.props;
+
+    return (
+      <div>
+        <h1>Thing (by {increment})</h1>
+        <div>
+          <label>Count:</label>
+          <span>{total}</span>
+        </div>
+        <button className="btn btn-default" onClick={this.click}>
+          Add
+        </button>
+      </div>
+    );
+  };
+}
+
 
 Thing.propTypes = {
   increment: PropTypes.number,
+  // withTotal
+  add: PropTypes.func,
+  total: PropTypes.number,
 }
 
 Thing.defaultProps = {
   increment: 1,
 };
 
-export default Thing;
+export default withTotal()(
+  Thing
+);
